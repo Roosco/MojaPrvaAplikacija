2a1bfee027879010105c83bf65d92925fd78cd7d
Roosco <rusac.dominik@gmail.com>
2018-02-15 23:34:24 +0100

    Initial commit

---
+++ app/src/main/java/rusac/mojaprvaaplikacija/DisplayMessageActivity.java
@@ -0,0 +1,26 @@
+package rusac.mojaprvaaplikacija;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static rusac.mojaprvaaplikacija.MainActivity.EXTRA_MESSAGE;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
